c335aef790e7e4643408f78bc89da159c515346d
Fabio Rogerio <fabiorogerio14@gmail.com>
2022-09-13 17:16:15 -0300

    inicial project MyPage

---
+++ teste.js
@@ -0,0 +1,22 @@
+/*
+Inicio
+Escreva v1
+Ler v1
+Escreva v2
+Ler v2
+
+Soma = v1 + v2
+
+Escreva soma
+Fim
+*/
+
+var v1;
+v1=console.log(prompt('Variavel 1:'));
+var v2;
+v2=console.log(prompt('Variavel 1:'));
+
+var Soma;
+Soma = v1 + v2;
+
+console.log(Soma);
